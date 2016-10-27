8f423ed24a8a495692bcb3d38328c1aa847c251d
Thomas White <tdwhite1@sheffield.ac.uk>
2016-10-27 13:35:46 +0100

    New script for generating data

--- data-analyzer.R
+++ data-analyzer.R
@@ -13,8 +13,16 @@ k_means <- function(data, centers){
 }
 
 write_centers_to_file <- function(centers, filename){
-  centers <- t(cbind(rownames(centers), centers))
+  #centers <- t(cbind(rownames(centers), centers))
   write(centers, file=filename, sep=',', ncolumns=nrow(centers))
+}
+
+write_sequences_to_file <- function(seqs, filename){
+  write(seqs, file=filename, sep=',', ncolumns=length(seqs))
+}
+
+append_sequences_to_file <- function(seqs, filename){
+  write(seqs, file=filename, sep=',', ncolumns=length(seqs), append=TRUE)
 }
 
 write_clusters_to_file <- function(named_clusters, filename){
@@ -81,10 +89,13 @@ generate_data <- function(file_start, extension, data, clusters, n, sequences, s
   
   raw_data <- data[sapply(data, is.numeric)]
   names = data[1]
-  print("Finished analysing data, performing K-Means Clustering.")
-  means <- k_means(raw_data, clusters)
-  write_centers_to_file(means$centers, paste('processed/', file_start, extension, sep=""))
-  named_clusters <- unlist(t(cbind(names, means$cluster)))
+  print(paste("K-Means Clustering:", extension))
+  means <- cluster::clara(raw_data, clusters, pamLike=TRUE)
+  meds <- means$medoids
+  meds <- cbind(1:clusters, meds)
+  meds <- t(meds)
+  write_centers_to_file(meds, paste('processed/', file_start, extension, sep=""))
+  named_clusters <- unlist(t(cbind(names, means$clustering)))
   #write_clusters_to_file(named_clusters, paste('processed/', file_start, '-', clusters, '-', n, '.classificationdata', sep=""))
   n_c <- cbind(names, means$cluster)
   print("K-Means complete. Data written to files. Starting cluster replacement.")
@@ -92,30 +103,52 @@ generate_data <- function(file_start, extension, data, clusters, n, sequences, s
   n_match <- match(n_c[,1], sequences)
   n_match <- n_match[!is.na(n_match)]
   n_seq <- replace(sequences, n_match, n_c[,2])
+  write_sequences_to_file(n_seq, paste('processed/', file_start, ".raw_sequence", extension, sep=""))
+  append_sequences_to_file(sequences, paste('processed/', file_start, ".raw_sequence", extension, sep=""))
+  
+  rowNum <- nrow(states)
+  
+  print(paste("- Sequence Writing Complete! Starting State Replacement of", rowNum, "states."))
+  
   r_states = read.table(text="",
                         colClasses = c("character", "character"),
                         col.names = c("state", clusters))
   
   output_string <- ""
-  for (i in 1:length(states[,2])){
-    ss <- unlist(strsplit(as.character(states[i,2]), "[,]"))
-    ss <- ss[!is.na(ss)]
-    ss <- ss[!is.null(ss)]
-    n_match <- match(n_c[,1], ss)
-    n_match <- n_match[!is.na(n_match)]
-    sT <- table(unlist(replace(ss, n_match, n_c[,2])))
-    stateT <- paste(states[i, 1], sep="")
-    table_out <- capture.output(write.table(sT, "", col.names = FALSE, sep="#", quote = FALSE, row.names = FALSE, eol=","))
-    
-    output <- stateT
-    output_string <- paste(output_string, paste(stateT), sep="\n")
-    
-    output_string <- paste(output_string, table_out, sep=":")
-    
+  if (rowNum > 2){
+    for (i in 1:rowNum){
+      ss <- unlist(strsplit(as.character(states[i,2]), "[,]"))
+      ss <- ss[!is.na(ss)]
+      ss <- ss[!is.null(ss)]
+      n_match <- match(ss, n_c[,1])
+      n_match <- n_match[!is.na(n_match)]
+      sT <- table(unlist(n_c[n_match,]['means$cluster']))
+      stateT <- paste(states[i, 1], sep="")
+      table_out <- capture.output(write.table(sT, "", col.names = FALSE, sep="#", quote = FALSE, row.names = FALSE, eol=","))
+      
+      output <- stateT
+      output_string <- paste(output_string, paste(stateT), sep="\n")
+      
+      output_string <- paste(output_string, table_out, sep=":")
+      
+      remove(output)
+      remove(table_out)
+      remove(n_match)
+      remove(ss)
+      remove(sT)
+      remove(stateT)
+    }
   }
+  
+  print("- Writing State Replacement")
+  
   sink(paste('processed/', file_start, '.state', extension, sep=''))
   print(cat(output_string), quote = FALSE, row.names = FALSE)
   sink()
+  
+  remove(r_states)
+  remove(output_string)
+  remove(raw_data)
   
   print("Replacement complete, calculating N-Grams.")
   occurances <- n_sequences(n, n_seq)
@@ -134,6 +167,8 @@ load_states <- function(filename){
     states <- states[,1:2]
   }
   close(conn)
+  remove(lines)
+  remove(conn)
   return(states)
 }
 
@@ -168,21 +203,28 @@ process <- function(file_starts, clusters, n, filename){
   print(ngramhand, full=TRUE)
   sink()
   
+  remove(ngramhand)
+  
   ngramposition <- generate_data(filename, '.hand_position_data', positiondata, clusters, n, seqdata, states)
   sink(paste('processed/', filename,'.hand_position_ngram', sep=''))
   print(ngramposition, full=TRUE)
   sink()
   
+  remove(ngramposition)
+  
   ngramrotation <- generate_data(filename, '.hand_rotation_data', rotationdata, clusters, n, seqdata, states)
   sink(paste('processed/', filename, '.hand_rotation_ngram', sep=''))
   print(ngramrotation, full=TRUE)
   sink()
+  
+  remove(ngramrotation)
 
   ngGestures <- ngram(gestures, n=n)
   sink(paste('processed/', filename, '.gesture_type_ngram', sep=''))
   print(ngGestures, full=TRUE)
   sink()
-
+  
+  write_sequences_to_file(gestures, paste('processed/', filename, '.raw_sequence.gesture_type_data', sep=''))
   
   output_string <- ""
   for (i in 1:length(states_gestures[,2])){
@@ -203,8 +245,13 @@ process <- function(file_starts, clusters, n, filename){
   print(cat(output_string), quote = FALSE, row.names = FALSE)
   sink()
   
+  remove(output_string)
+  remove(states_gestures)
+  
+  
 }
 
 process_all <- function(file_starts, filename){
-  process(file_starts, 100, 2, filename)
+    process(file_starts, 100, 2, paste(filename, sep=""))
+    rm(list=ls(all=TRUE))
 }
