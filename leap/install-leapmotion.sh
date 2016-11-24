88ba1cc87956f27fb882a0749789bba5f1c4fc1a
Thomas D. White <tdwhite1@sheffield.ac.uk>
2016-11-24 18:15:03 +0000

    changes to make travis build work

--- leap/install-leapmotion.sh
+++ leap/install-leapmotion.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=LeapJava.jar -DgroupId=com.leapmotion -DartifactId=leapmotion.sdk -Dversion=1.0 -Dpackaging=JAR
+mvn install:install-file -Dfile=LeapJava.jar -DgroupId=com.leapmotion -DartifactId=leapmotion.sdk -Dversion=1.0 -Dpackaging=jar
