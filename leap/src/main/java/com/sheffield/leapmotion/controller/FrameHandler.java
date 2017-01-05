02c02bf955571c135d473a8c20c45cd9031cd588
Thomas D. White <tdwhite1@sheffield.ac.uk>
2017-01-05 13:54:03 +0000

    Fixed a bug where UserPlayback was using random gestures

--- leap/src/main/java/com/sheffield/leapmotion/controller/FrameHandler.java
+++ leap/src/main/java/com/sheffield/leapmotion/controller/FrameHandler.java
@@ -234,7 +234,7 @@ public class FrameHandler implements Tickable {
                 fl.onFrameSwitch(last, next);
             }
             return;
-        } else {
+        } else if (!(frameGenerator instanceof UserPlaybackFrameGenerator) {
             if (!(frame instanceof  SeededFrame)) {
                 frame = new SeededFrame(frame);
             }
